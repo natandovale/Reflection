0e2bec8311d191256e0a6a2e4b7750fecfbb80ad
natanaeloliveira <natanaeloliveira@mobills.com.br>
2022-01-09 13:55:54 -0300

    Criando convecoes para acessar nosso assembly

---
+++ Bytebank.Portal/Bytebank.Portal/Infraestrutura/Utilidades.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bytebank.Portal.Infraestrutura
+{
+    public static class Utilidades
+    {
+        public static string ConverterPathParaNomeAssembly(string path)
+        {
+            var prefixoAssembly = "Bytebank.Portal";
+            var pathComPontos = path.Replace("/", ".");
+            var nomeCompleto = $"{prefixoAssembly}{pathComPontos}";
+            return nomeCompleto;
+        }
+
+        public static string ObterTipoDeConteudo(string path)
+        {
+            if (path.EndsWith(".css"))
+                return "text/css; charset=utf-8";
+            if(path.EndsWith(".js"))
+                return "application/js; charset=utf-8";
+            if (path.EndsWith(".html"))
+                return "text/html; charset=utf-8";
+            throw new NotImplementedException("Tipo de conteudo não previsto!!!");
+        }
+    }
+}
--- Bytebank.Portal/Bytebank.Portal/Infraestrutura/WebApplication.cs
+++ Bytebank.Portal/Bytebank.Portal/Infraestrutura/WebApplication.cs
@@ -40,42 +40,21 @@ namespace Bytebank.Portal.Infraestrutura
             var path = requisicao.Url.AbsolutePath;
 
 
-            //Retorna o nosso documento styles.cs ou main.js
-            if (path == "/Assets/css/styles.css")
-            {
-                //responsável por retornar o Assembly em execução
-                var assembly = Assembly.GetExecutingAssembly();
-                var nomeResouce = "Bytebank.Portal.Assets.css.styles.css";
-                var resourceStream = assembly.GetManifestResourceStream(nomeResouce);
-                var bytesResouce = new byte[resourceStream.Length];
-                resourceStream.Read(bytesResouce, 0, (int)resourceStream.Length);
+            //responsável por retornar o Assembly em execução
+            var assembly = Assembly.GetExecutingAssembly();
+            //Transforma endereco da requisicao em um endereco dentro do nosso assembly(DLL/EXE)
+            var nomeResouce = Utilidades.ConverterPathParaNomeAssembly(path);
+            var resourceStream = assembly.GetManifestResourceStream(nomeResouce);
+            var bytesResouce = new byte[resourceStream.Length];
+            resourceStream.Read(bytesResouce, 0, (int)resourceStream.Length);
 
-                resposta.ContentType = "text/css; charset=utf-8";
-                resposta.StatusCode = 200;
-                resposta.ContentLength64 = resourceStream.Length;
+            resposta.ContentType = Utilidades.ObterTipoDeConteudo(path);
+            resposta.StatusCode = 200;
+            resposta.ContentLength64 = resourceStream.Length;
 
-                resposta.OutputStream.Write(bytesResouce, 0, bytesResouce.Length);
+            resposta.OutputStream.Write(bytesResouce, 0, bytesResouce.Length);
 
-                resposta.OutputStream.Close();
-            }
-            else if (path == "/Assets/js/main.js")
-            {
-                //responsável por retornar o Assembly em execução
-                var assembly = Assembly.GetExecutingAssembly();
-                var nomeResouce = "Bytebank.Portal.Assets.js.main.js";
-                var resourceStream = assembly.GetManifestResourceStream(nomeResouce);
-                var bytesResouce = new byte[resourceStream.Length];
-                resourceStream.Read(bytesResouce, 0, (int)resourceStream.Length);
-
-                resposta.ContentType = "application/js; charset=utf-8";
-                resposta.StatusCode = 200;
-                resposta.ContentLength64 = resourceStream.Length;
-
-                resposta.OutputStream.Write(bytesResouce, 0, bytesResouce.Length);
-
-                resposta.OutputStream.Close();
-            }
-
+            resposta.OutputStream.Close();
             httpListener.Stop();
         }
     }
